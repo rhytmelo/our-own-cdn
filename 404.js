3d952da7fc1accd7bcae5527c7c22cbbe4c7e841
iamrhythm <rhythmiscoding@gmail.com>
2025-01-16 18:43:30 +0530

    404 page subscri

---
+++ 404.js
@@ -0,0 +1,9 @@
+// Author: RHYTMELO
+// Date: 2025-01-16
+// JavaScript for 404 page (if needed)
+
+document.querySelector('.back-link').addEventListener('click', (e) => {
+    e.preventDefault();
+    history.back();
+  });
+  
